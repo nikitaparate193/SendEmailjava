bef9813a52fe0b3fee5143a17bc6e7a3e853ac6c
Nikita Parate <niksparate2@gmail.com>
2023-06-11 12:21:24 +0530

    SendEmailjava

---
+++ src/main/java/org/example/CustomizedMailAuthentication.java
@@ -0,0 +1,11 @@
+package org.example;
+import javax.mail.Authenticator;
+import javax.mail.PasswordAuthentication;
+
+public class CustomizedMailAuthentication extends Authenticator {
+
+    protected PasswordAuthentication getPasswordAuthentication()
+    {
+        return new PasswordAuthentication(MailMetaData.myUserMail,MailMetaData.password);
+    }
+}
---
+++ src/main/java/org/example/MailMetaData.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class MailMetaData {
+    public static final String HostServer = "smtp.gmail.com";
+    public static final String port = "465";
+    public static final String sslProperty = "mail.smtp.ssl.enable";
+    public static final String authPerm = "mail.smtp.auth";
+
+    public static final String myUserMail = "niksparate2@gmail.com";
+
+    public static final String password = "buyikzttthlzlplu";// app password
+
+    public static final String receiverMail = "mahajanswati922@gmail.com";
+}
